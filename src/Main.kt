0747e2fc3d083eec9205d98fd63dcdf0f3a051b3
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2020-05-21 19:27:01 +0530

    Make appropriate changes to get the desired result.

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,17 @@
 fun main() {
     val robot = Robot("Sam")
+    println("____________________________________________________________________________________________________________________________________________________________________")
     robot.ringAlarm("7:00 A.M.")
+    println()
     robot.makeCoffee("1 Spoon")
+    println()
     robot.heatWater("112 degrees")
+    println()
     robot.packBag(setTimeTable())
+    println()
     var breakfastList = arrayListOf("Poha", "Sandwich", "Fried Rice", "Cutlets", "Maggi", "Cornflakes")
     robot.cookFoodForBreakfast(breakfastList)
+    println()
     var lunchList = arrayListOf(
         "Stuffed Parantha",
         "Rajma Chawal",
@@ -15,8 +21,12 @@ fun main() {
         "Sambhar-Dosa"
     )
     robot.cookFoodForLunch(lunchList)
+    println()
     robot.ironClothes("Black Jeans and White T-shirt")
+    println("____________________________________________________________________________________________________________________________________________________________________")
 }
+
+//setTimeTable() method to input the time table according to the day.
 private fun setTimeTable(): ArrayList<List<String>> {
     var timeTableList = arrayListOf<List<String>>(arrayListOf<String>())
     timeTableList.add(arrayListOf(" ", "8:00 - 9:00", "9:00 - 10:00", "10:00 - 11:00", "11:00 - 12:00", "12:00 - 1:00"))
--- src/Robot.kt
+++ src/Robot.kt
@@ -1,12 +1,15 @@
 class Robot(val robotName: String) {
 
+    //ringAlarm() method to ring the alarm on time.
     fun ringAlarm(time: String) {
         var dayList = arrayListOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
         println("Alarm is ringing at $time on ${dayList.toString()}!")
     }
 
+    //data class CoffeeIngredients to give data of the other ingredients to be used in the coffee.
     data class CoffeeIngredients(var waterAmt: String, var coffeeAmt: String)
 
+    //makeCoffee() method to make coffee according to the desire for the day.
     fun makeCoffee(sugarAmt: String) {
         var blackCoffee = arrayListOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
         var coffeeWithMilk = arrayListOf("Saturday", "Sunday")
@@ -15,29 +18,39 @@ class Robot(val robotName: String) {
         println("Coffee with milk is made at ${coffeeWithMilk.toString()} with ${coffeeIngredients.waterAmt} of water, ${coffeeIngredients.coffeeAmt} of coffee and $sugarAmt of sugar!")
     }
 
+    //heatWater() method to heat the water upto temperature suitable for bathing.
     fun heatWater(bathingTemp: String) {
         var dayList = arrayListOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
         println("Take bath at ${dayList.toString()} with water at temperature $bathingTemp!")
     }
 
+    //packBag() method to put the books in bag according to the time table of particular day.
     fun packBag(timeTable: ArrayList<List<String>>) {
         var dayList = arrayListOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
         for (j in 1..6) {
             for (i in dayList) {
-                if (i == timeTable[j][0])
-                    println("Bag is packed according to the timetable ${timeTable[j].toString()} of the day $i!")
+                if (i == timeTable[j][0]) {
+                    print("Bag is packed according to the timetable [")
+                    for (k in 1..4) {
+                        print("${timeTable[j][k]}, ")
+                    }
+                    println("${timeTable[j][5]}] of the day $i!")
+                }
             }
         }
     }
 
+    //cookFoodForBreakfast() method to cook the food for breakfast.
     fun cookFoodForBreakfast(breakfastFoodList: List<String>) {
         println("Breakfast cooked for today is ${breakfastFoodList.random()}!")
     }
 
+    //cookFoodForLunch() method to cook the food for lunch.
     fun cookFoodForLunch(LunchFoodList: List<String>) {
         println("Lunch cooked for today is ${LunchFoodList.random()}!")
     }
 
+    //ironClothes() method to iron the clothes and make them ready to wear.
     fun ironClothes(clothes: String) {
         println("$clothes are ironed and ready to wear!")
     }
